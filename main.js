669fbf6e762d94b7c26ee9156a64e01109dedf5e
Noah <38698485+noahreinalter@users.noreply.github.com>
2021-09-14 13:36:52 +0200

    Added logging

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const { promises: fs } = require('fs')
 async function run() {
     try {
         const path = core.getInput('path')
+        console.log(path)
         let content = await fs.readFile(path, 'utf8')
 
         const regexPattern = core.getInput('regex_pattern')
+        console.log(regexPattern)
         regex = new RegExp(regexPattern);
 
         const match = content.match(regex)
