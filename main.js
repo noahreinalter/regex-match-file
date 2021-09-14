8905d94a20535b375f8ccc3689a1ae4ada909e2b
Noah <38698485+noahreinalter@users.noreply.github.com>
2021-09-14 13:13:50 +0200

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const { promises: fs } = require('fs')
+
+async function run() {
+    try {
+        const path = core.getInput('path')
+        let content = await fs.readFile(path, 'utf8')
+
+        const regexPattern = core.getInput('regex_pattern')
+        regex = new RegExp(regexPattern);
+
+        const match = content.match(regex)
+
+        if (match == null) {
+            core.setFailed('The regex failed to match')
+        }
+    } catch(error) {
+        core.setFailed(`Action failed with error ${err}`);
+    }
+}
+
+run()
